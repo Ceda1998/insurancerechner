b597573ad336af7c4d980a7a99ab22dcf41bc69f
Cedric Daunke <cedricdaunke@ip-192-168-178-40.eu-central-1.compute.internal>
2024-03-22 17:19:27 +0100

    finished bmi calculator js and added more design

--- js/bmi.js
+++ js/bmi.js
@@ -1,16 +1,49 @@
-let currentAge = document.getElementById('currentAge');
-let currentWeight = document.getElementById('currentWeight');
-
-
+let bmiForm = document.getElementById('bmiCalculatorForm');
+let currentAge = document.getElementById("currentAge");
+let currentWeight;
+let currentHeight;
+let currentHealthStatus = document.getElementById("currentStatus");
+let yourBmi = document.getElementById("yourCurrentBmi");
+let bmi;
 
 function calculateBmi() {
-
-}
-
-function renderBmiNumber() {
+  currentWeight = document.getElementById("currentWeight").value;
+  currentHeight = document.getElementById("currentHeight").value;
+  bmi = Math.round(currentWeight / Math.pow(currentHeight / 100, 2));
+  yourBmi.innerHTML = `${bmi}`;
+  renderBmiHealthStatus();
+  bmiForm.reset();
 
 }
 
 function renderBmiHealthStatus() {
-    
+    let status;
+    if (bmi < 18.5) {
+        status = "Du bist untergewichtig";
+    } else if (bmi >= 18.5 && bmi < 25) {
+        status = "Normales Gewicht";
+    } else if (bmi >= 25 && bmi < 30) {
+        status = "Du bist leicht übergewichtig";
+    } else if (bmi >= 30 && bmi < 35) {
+        status = "Du bist übergewichtig";
+    } else {
+        status = "Du bist adipös"
+    }
+    currentHealthStatus.innerHTML = ` (${status})`;
+}
+
+function checkCheckboxes() {
+    let male = document.getElementById('maleCheckbox');
+    let female = document.getElementById('femaleCheckbox');
+    let divers = document.getElementById('diversCheckbox');
+
+    if (male.checked == true) {
+        female.checked = false;
+        female.disabled = true;
+        divers.checked = false;
+        divers.disabled = true;
+    } else {
+        female.disabled = false;
+        divers.disabled = false;
+    }
 }
--- js/mainscript.js
+++ js/mainscript.js
@@ -1,3 +1,7 @@
 function openCalculator(location) {
     window.location.href = `${location}`
 }
+
+function backToStart() {
+    window.location.href = "index.html";
+}
